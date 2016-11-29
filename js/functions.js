976d126a2ad171dfb3d0231c1ec54c280d426f00
szhicay <szhicay@hotmail.com>
2016-11-29 11:29:57 -0500

    primer commit pagina web

---
+++ js/functions.js
@@ -0,0 +1,22 @@
+$(function(){
+  $("#header").load("header.html");
+  $("#footer").load("footer.html");
+});
+
+
+//
+// function initMap() {
+//   // -0.117545, -78.507487
+//   var myLatLng = {lat: -0.117545, lng: -78.507487};
+//
+//   var map = new google.maps.Map(document.getElementById('map'), {
+//     zoom: 4,
+//     center: myLatLng
+//   });
+//
+//   var marker = new google.maps.Marker({
+//     position: myLatLng,
+//     map: map,
+//     title: 'Hello World!'
+//   });
+// }
---
+++ php/funcion.php
@@ -0,0 +1,21 @@
+
+<?php
+    function contador()
+    {
+        $archivo = "contador.txt"; //el archivo que contiene en numero
+        $f = fopen($archivo, "r"); //abrimos el archivo en modo de lectura
+        if($f)
+        {
+            $contador = fread($f, filesize($archivo)); //leemos el archivo
+            $contador = $contador + 1; //sumamos +1 al contador
+            fclose($f);
+        }
+        $f = fopen($archivo, "w+");
+        if($f)
+        {
+            fwrite($f, $contador);
+            fclose($f);
+        }
+        return $contador;
+    }
+?>
